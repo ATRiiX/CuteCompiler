c271239846f7847ea9a75e6be9857400ab480371
peter <xdf780885029@gmail.com>
2019-06-17 15:42:47 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
+#include <fstream>
+#include "ASTNodes2.h"
+#include "CodeGen.h"
+#include "ObjGen.h"
 #include "json.hpp"
+
 using namespace std;
+using json = nlohmann::json;
+
 int main(int argc, char **argv) {
-    cout<<"test"<<endl;
+    cout << "test start " << endl;
+    string jsonFile = "visualization/new_tree.json";
+
+    // create an empty structure (null)
+    json j;
+
+// add a number that is stored as double (note the implicit conversion of j to an object)
+    j["pi"] = 3.141;
+
+// add a Boolean that is stored as bool
+    j["happy"] = true;
+
+// add a string that is stored as std::string
+    j["name"] = "Niels";
+
+// add another null object by passing nullptr
+    j["nothing"] = nullptr;
+
+// add an object inside the object
+    j["answer"]["everything"] = 42;
+
+// add an array that is stored as std::vector (using an initializer list)
+    j["list"] = {1, 0, 2};
+
+// add another object (using an initializer list of pairs)
+    j["object"] = {{"currency", "USD"},
+                   {"value",    42.99}};
+
+// instead, you could also write (which looks very similar to the JSON above)
+    json j2 = {
+            {"pi",      3.141},
+            {"happy",   true},
+            {"name",    "Niels"},
+            {"nothing", nullptr},
+            {"answer",  {
+                                {"everything", 42}
+                        }},
+            {"list",    {       1, 0, 2}},
+            {"object",  {
+                                {"currency",   "USD"},
+                                   {"value", 42.99}
+                        }}
+    };
+
+    std::ofstream outFile(jsonFile);
+    outFile << std::setw(4) << j << std::endl;
+
+    std::cout << j.dump(4) << std::endl;
 }
