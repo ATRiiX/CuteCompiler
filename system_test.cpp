46403f73b9d8261893e82003e7c4e7a0081b02b2
peter <xdf780885029@gmail.com>
2019-06-20 14:59:42 +0800

    update

--- system_test.cpp
+++ system_test.cpp
@@ -6,10 +6,14 @@
 rm test
 g++ -o system_test system_test.cpp
 ./system_test
-
+./test
 */
 using namespace std;
 int main(int argc, char **argv) {
-    system("clang++ -O3  output.o -o test ");
+    string command = "clang++ -O3 -o";
+    string inputfilename = "output.o";
+    string outputfilename = "test";
+    system((command+outputfilename+inputfilename).c_str());
+   // system("clang++ -O3  output.o -o test ");
     return 0;
 }
