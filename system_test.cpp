b029cc07bac251c149f7b2651493522e0af6811c
peter <xdf780885029@gmail.com>
2019-06-20 14:36:07 +0800

    fix bug

--- system_test.cpp
+++ system_test.cpp
@@ -2,8 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+rm test
+g++ -o system_test system_test.cpp
+./system_test
+
+*/
 using namespace std;
 int main(int argc, char **argv) {
-    system("clang++ -O3  output.o -o test ")
+    system("clang++ -O3  output.o -o test ");
     return 0;
 }
